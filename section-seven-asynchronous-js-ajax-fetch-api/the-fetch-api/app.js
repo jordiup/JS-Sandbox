c1bda319b794405b2fb3f729135e3d648c84753f
jordiup <jordi.hermoso.up@gmail.com>
2019-02-13 12:11:00 +0800

    Section 7 - Fetch API, recreating easyHTTP library but with fetch

---
+++ section-seven-asynchronous-js-ajax-fetch-api/the-fetch-api/app.js
@@ -0,0 +1,59 @@
+document.getElementById('button1').addEventListener('click', getText);
+
+document.getElementById('button2').addEventListener('click', getJSON);
+
+document.getElementById('button3').addEventListener('click', getExternal);
+
+// Get local text file data
+function getText() {
+  fetch('test.txt')
+    .then(function(res){
+      return res.text();
+    })
+    .then(function(data){
+      console.log(data);
+      document.getElementById('output').innerHTML = data;
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+}
+
+// Get local JSON data
+function getJSON() {
+  fetch('customer.json')
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data){
+      console.log(data);
+      let output = '';
+      data.forEach(function(customer) {
+        output += `<li>${customer.name}, ${customer.company}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+}
+
+// Get external data
+function getExternal(){
+  fetch('http://api.github.com/users')
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data){
+      console.log(data);
+      let output = '';
+      data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+
+}
